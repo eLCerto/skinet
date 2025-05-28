f46fc4100973255852d9674fcccf6f8d2cf2c0dc
eLCerto <malisek.samuel@gmail.com>
2025-05-28 23:24:10 +0200

    End of course

---
+++ API/wwwroot/chunk-LPRCWL65.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-2RGVRMEW.js";import{j as o}from"./chunk-5FOGVBZY.js";import{ka as t,r as e,x as n}from"./chunk-55L6EDGV.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["/account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51RO0RXPJgXa7q2guRQ86YcKnhvp4y7yFNYDdCxRH5cWjZGBQpiE4EJamP8f6s9fGwF9tIMV1Uq0zSL0DQoGamLh00071zE6zvF'
+    stripePublicKey: 'pk_test_51RTTHE2SeY979ho7SQNlGk39HtjTDwO7Jn4KAKB8oHyfjBxbC1sUb4sxVT54aZTnG7NurPEoS4BsWBjx69wVB6wv00T07DDYDD'
 };
 
